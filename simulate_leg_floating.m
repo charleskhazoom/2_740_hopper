be0e4d2249084c7bc680d68ac5c593ad51a2998c
Charles Khazoom <charles.khazoom@hotmail.com>
2020-11-11 17:54:54 -0500

    make save_traj function for se hwan

--- simulate_leg_floating.m
+++ simulate_leg_floating.m
@@ -21,7 +21,7 @@ addpath(path_sym_dynamics)
     l_A_m3=0.0622;          l_C_m4=0.0610;
     N = 18.75;
     Ir = 0.0035/N^2;
-    g = 9.81;    
+    g = 0.81;    
     
     % parameters to be adjusted according to design
     m_body = 0.186+0.211;%
@@ -36,19 +36,21 @@ addpath(path_sym_dynamics)
 %     friction_coeff = 0.3;
     ground_height = 0;
     %% Parameter vector
+ motor_kt = 0.18;
+ motor_R = 2;
 
 p   = [m1 m2 m3 m4 m_body m_arm I1 I2 I3 I4 I_arm Ir N l_O_m1 l_B_m2...
-    l_A_m3 l_C_m4 l_cm_arm l_cm_body l_OA l_OB l_AC l_DE l_body l_arm g]';        % parameters
+    l_A_m3 l_C_m4 l_cm_arm l_cm_body l_OA l_OB l_AC l_DE l_body l_arm g motor_kt motor_R]';        % parameters
 
     
     %% Perform Dynamic simulation
-    tf = 0.8;
+    tf = 0.15;
 %     num_step = floor(tf/dt);
     tspan = [0 tf];
 %     tspan = linspace(0, tf, num_step); 
     
 %     z0 = [0;0.2; 10*pi/180; 10*pi/180; 0; 0;0;0;0;0]; %[-pi/4; pi/2; 0; 0];
-    desired_hip_pos0 = [0.014;0.15];
+    desired_hip_pos0 = [0.014;0.1];
     guess_leg_angle  = [10*pi/180; 10*pi/180];
     init_leg_angle = fsolve(@(x)solve_init_pose(x,desired_hip_pos0,p),guess_leg_angle);
     init_arm_angle=0;
@@ -67,8 +69,9 @@ p   = [m1 m2 m3 m4 m_body m_arm I1 I2 I3 I4 I_arm Ir N l_O_m1 l_B_m2...
     tout = [tout tsim(2:end)'];
     z_out = [z_out  zsim(2:end,:)'];
     
+    z0=zsim(end,:)';
     opts2 = odeset('events',@(t,z) detect_TD(t,z,p),'AbsTol',1e-6,'RelTol',1e-6);
-    [tsim,zsim,tTD,zTD] = ode45(@(t,z) get_dynamics_flight(t,z,p),[tsim(end):0.002:tspan(2)],zsim(end,:)',opts2);
+    [tsim,zsim,tTD,zTD] = ode45(@(t,z) get_dynamics_flight(t,z,p,z0),[tsim(end):0.002:tspan(2)],z0,opts2);
 
     tout = [tout tsim(2:end)'];
     z_out = [z_out  zsim(2:end,:)'];
@@ -79,7 +82,7 @@ for i =1:length(z_out)
         u_out(:,i) = control_law_stance(tout(i),z_out(:,i),p);
         
     else 
-        u_out(:,i) = control_law_flight(tout(i),z_out(:,i),p);
+        u_out(:,i) = control_law_flight(tout(i),z_out(:,i),p,z0);
     end
      
 end
@@ -102,22 +105,40 @@ fignb=1;
     %% make plots
 
     fignb = plot_solution(tout,z_out,u_out,tLO,p,fignb);
-
+    
+    %% save trajectoryname
+    name = 'dummy_traj_name'
+    save_traj(tout,z_out,u_out,name)
 end
 
 function tau = control_law_stance(t, z, p)
  
-    tau = [-0.5;-5;0];
+    tau = [0.5;-2;0];
+    tau = saturate_torque(z,tau,p);
 
 % 
 end
 
-function tau = control_law_flight(t, z, p)
+function tau = control_law_flight(t, z, p,z0)
  
     %tau = [0.5;0;0.5];
 %     tau = [-0.5;-0.5;1];
-    tau = [0;0;0];
-% 
+    
+    kp = 1;
+    kd = 0.5;
+    q_ref = z0(3:5);
+    q = z(3:5);
+    
+    qd = z(8:10);
+    
+    qd_ref = [0;0;0];
+
+    tau = kp*(q_ref-q) + kd*(qd_ref - qd);
+    tau = saturate_torque(z,tau,p);
+
+    
+%     tau = [0;0;0];
+
 end
 
 
@@ -146,7 +167,7 @@ function [dz,F] = get_dynamics_stance(t,z,p)
 end
 
 
-function [dz,F] = get_dynamics_flight(t,z,p)
+function [dz,F] = get_dynamics_flight(t,z,p,z0)
     
     % Get mass matrix
     A = A_floating(z,p);
@@ -154,7 +175,7 @@ function [dz,F] = get_dynamics_flight(t,z,p)
     % Compute Controls
     
     
-    tau = control_law_flight(t,z,p);
+    tau = control_law_flight(t,z,p,z0);
     
     % Get b = Q - V(q,qd) - G(q)
     F = [0;0]; % constraint force is zero in flight
@@ -175,7 +196,7 @@ function [value,isterminal,direction] = detect_LO(t,z,p)
     % recompute here: how to pass these variables from dynamics without
     % recomputing everything?
     A = A_stance(z,p);
-    tau = control_law_flight(t,z,p);
+    tau = control_law_stance(t,z,p);
     b = b_stance(z,tau,p);
 
         
@@ -201,4 +222,20 @@ function [value,isterminal,direction] = detect_TD(t,z,p)
     value = pos_foot(2);% detect when F cross zero
     direction  = -1; % when position is going down
     isterminal = 0; % = 1 : stop integration to change dynamics
+end
+
+function u = saturate_torque(z,u,p)
+    emax = 12;
+    kt = p(27); 
+    R  = p(28);
+    N  = p(13);
+    
+    qd = z(8:10);
+    
+    
+    tau_max = (emax - kt*qd*N)*kt/R*N;
+    tau_min = (-emax - kt*qd*N)*kt/R*N;
+    
+    u = max(min(u,tau_max),tau_min);
+    
 end
